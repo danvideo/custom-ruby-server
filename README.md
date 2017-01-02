17b3070525c0fa441fdf53aba8118a963fb0bade
Dan Schanler <dschanler@jibe.com>
2017-01-02 21:59:55 +0200

    added rescue

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Goal is to create a Ruby TCP Server that returns client IP Address in Morse Code
 - Added a few other things: regular ip, headers, server lists connections, etc...
 - Morse dictionary included
-- TODO: haven't included error handling
+- todo: better error handling depending on type of tcp connection problem?
 
 ### To start server: 
 `ruby custom-server.rb PORT_HERE` & `curl localhost:port` in a 2nd term.
--- custom-server.rb
+++ custom-server.rb
@@ -6,18 +6,22 @@ class CustomServer
   def start_server(listen_port)
     server = TCPServer.new listen_port
     connections = 0
+    begin
     loop do
       Thread.start(server.accept) do |client|
         connections += 1
         puts "Connection ##{connections} @ #{Time.now}" 
         sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr
         remote_header = client.recvmsg.first
-        client.puts "Hello !"
-        client.puts "This is your IP address: #{remote_ip}"
-        client.puts "This is your IP in Morse code: #{ip_to_morse(remote_ip)}"
-        client.puts "This is your request Header: \n#{remote_header}"
+        client.puts "\n\nHello! \n\n"
+        client.puts "This is your IP address: #{remote_ip}\n\n"
+        client.puts "This is your IP in Morse code: #{ip_to_morse(remote_ip)}\n\n"
+        client.puts "This is your request Header: \n\n#{remote_header}\n"
         client.close
       end
+    end
+    rescue Errno::ECONNRESET, Errno::EPIPE => e
+      puts e.message
     end
   end
 
@@ -29,5 +33,6 @@ class CustomServer
     output
   end
 end
-
-CustomServer.new.start_server(ARGV[0])
+ 
+puts "\n*Attention* please include a port # when calling the server\n\n" unless ARGV[0]
+CustomServer.new.start_server(ARGV[0]) if ARGV[0]
